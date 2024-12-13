adca7903497e543a3130da044c46fea643374e28
Buttters <duylk99@gmail.com>
2024-12-13 00:05:21 -0500

    Added movement that works with physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public CharacterController CharacterController;
     [SerializeField]
     public float MovementSpeed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CharacterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -28,26 +29,8 @@ public class Player : MonoBehaviour
             camForward = camForward.normalized;
             camRight = camRight.normalized;
 
-            // if (Input.GetAxisRaw("Horizontal") > 0)
-            // {
-            //     direction += Vector3.right;    
-            // }
-            // if (Input.GetAxisRaw("Horizontal") < 0)
-            // {
-            //     direction += Vector3.left;
-            // }
-            // if (Input.GetAxisRaw("Vertical") > 0)
-            // {
-            //     direction += Vector3.up;
-            // }
-            // if (Input.GetAxisRaw("Vertical") < 0)
-            // {
-            //     direction += Vector3.down;
-            // }
-
             Vector3 camRelativeMovement= camForward * playerVerticalInput + camRight * playerHorizontalInput;
-
-            transform.Translate(camRelativeMovement * MovementSpeed * Time.deltaTime, Space.World);
+            CharacterController.Move(camRelativeMovement * MovementSpeed * Time.deltaTime);            
         }
     }
 }
